04930f9581ab8d4b2a3897411ff24434cbebe5a6
je <august49822031@gmail.com>
2025-06-08 23:40:22 +0800

    Update build files with correct Vite build

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './css/index.css';
-import App from './App';
-createRoot(document.getElementById('root')).render(React.createElement(StrictMode, null,
-    React.createElement(App, null)));
--- dist/page/notFoundPage.js
+++
@@ -1,4 +0,0 @@
-import { Layout } from 'antd';
-export default function NotFoundPage() {
-    return React.createElement(Layout, null, " 404 Not Found");
-}
