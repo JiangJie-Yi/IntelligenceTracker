ef73b3e0842c2a535875706bb8496546b31bcf25
je <august49822031@gmail.com>
2025-06-10 00:15:29 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
